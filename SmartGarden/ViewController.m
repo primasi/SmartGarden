af25e16d883ef984e819144331ad8c74a7efbfdf
Primas, Ingo <primasi@mac-mini.local>
2017-07-28 12:34:16 +0200

    Initial Commit

---
+++ SmartGarden/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  SmartGarden
+//
+//  Created by Primas, Ingo on 28.07.17.
+//  Copyright © 2017 Bausparkasse Mainz AG. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
